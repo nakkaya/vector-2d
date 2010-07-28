de8c4dcd0fcc12c25cdd9ed7c8f92b4b9e26bcf7
Nurullah Akkaya <nurullah@nakkaya.com>
2010-07-28 16:16:20 +0300

    Initial Commit.

---
+++ src/vector_2d/core.clj
@@ -0,0 +1,126 @@
+(ns vector-2d.core
+  (:use [clojure.contrib.types :only (deftype)]
+	[clojure.contrib.generic :only (root-type)])
+  (:require [clojure.contrib.generic.arithmetic :as ga]
+	    [clojure.contrib.generic.comparison :as gc]))
+
+(defstruct vector-2d-struct :x :y)
+
+(deftype ::vector-2d vector-2d
+  (fn [x y] (struct vector-2d-struct x y))
+  (fn [v] (vals v)))
+
+(derive ::vector-2d root-type)
+
+(defmethod ga/+ [::vector-2d ::vector-2d]
+  [u v]
+  (let [[ux uy] (vals u)
+	[vx vy] (vals v)]
+    (vector-2d (ga/+ ux vx) (ga/+ uy vy))))
+
+(defmethod ga/- [::vector-2d ::vector-2d]
+  [u v]
+  (let [[ux uy] (vals u)
+	[vx vy] (vals v)]
+    (vector-2d (ga/- ux vx) (ga/- uy vy))))
+
+(defmethod ga/* [::vector-2d Object]
+  [u s]
+  (let [[ux uy] (vals u)]
+    (vector-2d (* ux s) (* uy s))))
+
+(defmethod gc/= [::vector-2d ::vector-2d]
+  [u v]
+  (let [[ux uy] (vals u)
+	[vx vy] (vals v)]
+    (and (gc/= ux vx) (gc/= uy vy))))
+
+(defn negative 
+  "Return a new vector in the opposite direction."
+  [u]
+  (let [[ux uy] (vals u)] 
+    (vector-2d (- ux) (- uy))))
+
+(defn magnitude 
+  "Return the length of the vector."
+  [v]
+  (let [[x y] (vals v)] 
+    (Math/sqrt (+ (* x x) (* y y)))))
+
+(defn dist 
+  "Return the distance between two vectors."
+  [u v]
+  (magnitude (ga/- u v)))
+
+(defn normalize 
+  "Returns the unit vector of the supplied vector."
+  [v]
+  (let [[x y] (vals v)
+	mag (magnitude v)] 
+    (if-not (= mag 0)
+      (vector-2d (/ x mag) (/ y mag)) (vector-2d 0 0))))
+
+(defn dot-product 
+  "See, http://en.wikipedia.org/wiki/Dot_product"
+  [u v]
+  (let [[ux uy] (vals u) [vx vy] (vals v)] 
+    (+ (* ux vx) (* uy vy))))
+
+(defn project 
+  "See, http://en.wikipedia.org/wiki/Vector_projection"
+  [u v]
+  (let [unit-v (normalize v)
+	dot (dot-product u unit-v)]
+    (ga/* unit-v dot)))
+
+(defn rotate 
+  "See, http://en.wikipedia.org/wiki/Rotation_(mathematics)"
+  [u angle]
+  (let [[ux uy] (vals u)
+	s (Math/sin (Math/toRadians angle))
+	c (Math/cos (Math/toRadians angle))]
+    (vector-2d (- (ga/* c ux) (ga/* s uy)) 
+	       (+ (ga/* s ux) (ga/* c uy)))))
+
+(defn bisect-angle 
+  "Returns the vector that lies halfway between vectors."
+  [u v]
+  (ga/+ (normalize u) (normalize v)))
+
+(defn in-view-cone? 
+  "Checks if the point is in viewing cone."
+  [view-pos direction angle point]
+  (let [vp (ga/- point view-pos)
+	cos (/ (dot-product vp direction)
+	       (ga/* (magnitude vp) (magnitude direction)))]
+    (> cos (Math/cos (Math/toRadians angle)))))
+
+(defn parallel? 
+  "Returns true if vectors are parallel to each other."
+  [u v]
+  (or (gc/= (normalize u) (normalize v))
+      (gc/= (normalize u) (negative (normalize v)))))
+
+(defn perpendicular? 
+  "Returns true if vectors are perpendicular to each other."
+  [u v]
+  (= 0 (Math/abs (dot-product u v))))
+
+(defn closest-point-on-line 
+  "Calculate a point on the line AB that is closest to point C."
+  [a b c]
+  (let [ac (ga/- c a)
+	ab (ga/- b a)
+	proj-mag (dot-product ac (normalize ab))]
+    (cond (< proj-mag 0) a
+	  (> proj-mag (magnitude ab)) b
+	  :default (ga/+ (project ac ab) a))))
+
+(defn line-circle-collision 
+  "Given line segment AB and circle C with radius R, returns true if 
+   circle collides with the line segmen."
+  [a b c r]
+  (let [closest (closest-point-on-line a b c)
+	distance (magnitude (ga/- c closest))]
+    (if (<= distance r)
+      true false)))
---
+++ test/vector_2d/core_test.clj
@@ -0,0 +1,48 @@
+(ns vector-2d.core-test
+  (:refer-clojure :exclude [+ - * =])
+  (:use (clojure.contrib.generic [arithmetic :only [+ - *]]
+				 [comparison :only [=]]))
+  (:use [vector-2d.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest arithmetic-test
+  (let [u (vector-2d 1 2)
+	v (vector-2d 2 2)] 
+    (is (= (vector-2d 3 4) (+ u v)))
+    (is (= (vector-2d 1 0) (- v u)))
+    (is (= (vector-2d 5 5) (* (vector-2d 1 1) 5)))
+    (is (= (vector-2d 5 5) (vector-2d 5 5)))))
+
+(deftest operations-test
+  (let [u (vector-2d 1 1)] 
+    (is (= (vector-2d -1 -1) (negative u)))
+    (is (= 1.4142135623730951 (magnitude u)))
+    (is (= 2.0 (dist (vector-2d 0 0) (vector-2d 2 0))))
+    (is (= (vector-2d 0.7071067811865475 0.7071067811865475) (normalize u)))
+    (is (= 2 (dot-product u u)))
+    (is (= (vector-2d 0.9999999999999998 0.9999999999999998)
+	   (project (vector-2d 1 1) (vector-2d 10 10))))
+    (is (= (vector-2d 6.123233995736766E-17 1.0)
+	   (rotate (vector-2d 1 0) 90)))
+    (is (= (vector-2d 5.0 0.0)
+	   (closest-point-on-line (vector-2d 1 0) (vector-2d 10 0) 
+				  (vector-2d 5 5))))
+    (is (= false
+	   (line-circle-collision (vector-2d 1 0) (vector-2d 10 0) 
+				  (vector-2d 5 5) 1)))
+    (is (= true
+	   (line-circle-collision (vector-2d 1 0) (vector-2d 10 0) 
+				  (vector-2d 5 5) 10)))
+    (is (= true (parallel? (vector-2d 2 -3) (vector-2d 8 -12))))
+    (is (= true (parallel? (vector-2d 1 2) (vector-2d 2 4))))
+    (is (= false (perpendicular? (vector-2d 1 5) (vector-2d 5 1))))
+    (is (= true (perpendicular? (vector-2d 0 5) (vector-2d 5 0))))
+    (is (= (vector-2d 1.0 1.0) 
+	   (bisect-angle (vector-2d 0 5) (vector-2d 5 0))))
+    (is (= true 
+	   (in-view-cone? (vector-2d 0 0) 
+			  (vector-2d 1 1) 90 (vector-2d 5 5))))
+    (is (= false (in-view-cone? (vector-2d 1 3) 
+				(vector-2d 1 1) 71 (vector-2d 3 2))))
+    (is (= true (in-view-cone? (vector-2d 1 3) 
+			       (vector-2d 1 1) 74 (vector-2d 3 2))))))
