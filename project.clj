a797b2a98ea854aa534c4e7711a57d9b7893cc1f
Nurullah Akkaya <nurullah@nakkaya.com>
2010-08-20 21:26:09 +0300

    switch to 1.2.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject vector-2d "1.0.0-SNAPSHOT"
   :description "Collection of 2D Vector Operations."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
