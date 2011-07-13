506f3cf4b406fcf11c0764b19ed6682a5fc5bc0a
Nurullah Akkaya <nurullah@nakkaya.com>
2011-07-13 17:51:55 +0300

    lein clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject vector-2d "1.0.0-SNAPSHOT"
   :description "Collection of 2D Vector Operations."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
