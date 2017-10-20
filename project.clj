1bb9d8c8cde2601a3ae997378239c02efc2bcf51
Nurullah Akkaya <nurullah@nakkaya.com>
2017-10-20 15:33:25 +0300

    add notice library moved

--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject vector-2d "1.0.0-SNAPSHOT"
-  :description "Collection of 2D Vector Operations."
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/algo.generic "0.1.2"]])
